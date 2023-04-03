3a219e2720ab6be33012874ba8183dd3e290a916
Vipul Malakar <118244485+vipulmalakar-poulimainfo@users.noreply.github.com>
2023-04-03 12:33:22 +0530

    real one

--- src/server/controller/transactions.js
+++ src/server/controller/transactions.js
@@ -7,7 +7,7 @@ const API_KEY = process.env.API_KEY;
 const AUTH_KEY = process.env.AUTH_KEY;
 
 Insta.setKeys(API_KEY, AUTH_KEY);
-Insta.isSandboxMode(true);
+Insta.isSandboxMode(false);
 
 exports.pay = (req, res) => {
     var name = req.body.data_name;
@@ -35,16 +35,16 @@ exports.pay = (req, res) => {
 };
 
 exports.getTransactionDetails = (req, res) => {
-    sdk.generateAccessTokenApplicationBasedAuthentication({
-      grant_type: 'client_credentials',
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET
-    }, {accept: 'application/json'})
-      .then(({ data }) => {
-        sdk.auth(`Bearer ${data.access_token}`);
-        sdk.getPaymentDetails1({id: req.body.payment_id })
-        .then(({ data }) => res.status(200).json({ data }))
-        .catch(err => res.status(500).json({ message: "Payment Failed" }));
-      })
+  sdk.generateAccessTokenApplicationBasedAuthentication({
+    grant_type: 'client_credentials',
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET
+  }, {accept: 'application/json'})
+    .then(({ data }) => {
+      sdk.auth(`Bearer ${data.access_token}`);
+      sdk.getPaymentDetails1({id: req.body.payment_id })
+      .then(({ data }) => res.status(200).json({ data }))
       .catch(err => res.status(500).json({ message: "Payment Failed" }));
+    })
+    .catch(err => res.status(500).json({ message: "Payment Failed" }));
 }
